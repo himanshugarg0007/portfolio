9a9d6b881570110c0e49c1f5336944a52393294e
Himanshugarg0007 <hunnygarg.bboy@gmail.com>
2023-12-08 20:12:14 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-
-import './App.css'
 import Navbar from './Components/Navbar'
 import Maincomp from './Components/Maincomp'
 import ScrollButton from './Components/Scrollbtn'
-import { useState, useEffect } from 'react'
 
 function App() {
-  const [isRendered, setIsRendered] = useState(false);
-
-  useEffect(() => {
-    if (document.querySelector('#root') !== null) {
-      setIsRendered(true);
-    }
-  }, []);
-
-  if (isRendered) {
-    const script = document.createElement("script");
-
-    script.src = "https://embeddable-chatbot.s3.ap-south-1.amazonaws.com/index.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-  }
 
 
   return (
@@ -29,7 +10,6 @@ function App() {
     <Navbar />
      <Maincomp />
      <ScrollButton/>
-     <div id="megachatbot"></div>
     </>
   )
 }
